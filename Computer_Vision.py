2124dec50fdd78d88bdcb638c9bbe84f46a86ec4
Eliwebs <77858515+Eliwebs@users.noreply.github.com>
2023-11-12 15:57:15 -0500

    Working with TF.Next - replace TF w/ our functions

--- Computer_Vision.py
+++ Computer_Vision.py
@@ -10,6 +10,7 @@ from keras.datasets import mnist
 # test = testing data
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
+
 # number of train labels for reference
 unique, counts = np.unique(y_train, return_counts=True)
 print("Train labels: ", dict(zip(unique, counts)))
@@ -32,7 +33,74 @@ for i in range(len(Randnums)):
     plt.imshow(image, cmap='gray')
     plt.axis('off')
     
-plt.show()
+#plt.show()
+
+
+#---------------------------------------------------------------------------------------------------------------------------
+
+
+
+# PREPROCESSING
 
 # normalize pixel values between 0-1
 x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# Flatten data set to single dimension array of size 'counts' by total # of pixels 
+# counts for train = 60000, for test = 10000, # of pixels is 28x28 = 784 per image
+train_flat, test_flat = x_train.reshape(x_train.shape[0], -1), x_test.reshape(x_test.shape[0], -1)
+
+
+
+# checking that works ^^^^^ (It does :)) 
+# train_flat = (60000, 784)
+# test_flat = (10000, 784)
+print(train_flat.shape)
+print('\n', test_flat.shape, '\n')
+
+
+
+# Function for generating random weights for training data - not currently being used
+def generate_training_wt(x, y):
+    empty = []
+    x = x_train
+    y = y_train
+    for i in range(x * y):
+        empty.append(np.random.randn())
+    return(np.array(empty).reshape(x, y))
+
+
+
+#------------------------------------------------------------------------------------------------------------------------------
+
+
+
+# Activation function (could directly use keras library - tf.keras.layers.Dense, activation = sigmoid, relU, etc)
+def relUActivation(x):
+    return(tf.maximum(0.0, x))
+
+# Building model of NN
+# Dense gives activation(dot product of (input, weights) + bias) = output for total layers
+# 256 layers are chosen because it provides better accuracy than 128, and about the same as >256.
+
+NNmodel = tf.keras.Sequential([tf.keras.layers.Dense(256, activation = relUActivation), 
+# Second Dense for final layer, total classifiers possible (0-9 is 10 numbers)
+                               tf.keras.layers.Dense(10)])
+
+
+# Compile model using adam optimizer, and SparseCategoricalCrossentropy
+# loss function Sparse.... is used because data is a flattened integer array, rather than one-hot
+NNmodel.compile(optimizer = 'adam', 
+                loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits = True), 
+                metrics=['accuracy'])
+
+# Training model with 10 epochs
+NNmodel.fit(train_flat, y_train, epochs = 10)
+
+
+# Testing accuracy and loss on test data
+
+loss, accuracy = NNmodel.evaluate(test_flat, y_test, verbose = 2)
+
+print(f'\nTest loss', loss)
+print(f'\nTest accuracy', accuracy)
+
